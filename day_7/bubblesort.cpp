6a9144cfbdcb1c1b636109c69edffebaf386ebf6
itsnikhil404 <25112cn258@glbitm.ac.in>
2026-06-06 17:42:30 +0530

    Create bubblesort.cpp

---
+++ day_7/bubblesort.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+void sortedarray(int arr[],int n){
+    for(int i= 0 ;i < n-1;i++){
+    for(int i = 0 ; i < n-1 ; i++){
+        if(arr[i]>arr[i+1]){
+            swap(arr[i],arr[i+1]);
+        } 
+    }
+}
+
+}
+
+int main (){
+    int n =6 ;
+    int arr[100]={10,1,7,6,14,9};
+    sortedarray(arr,6);
+    for(int i = 0 ; i<n ;i++){
+        cout << arr[i]<<" " ;
+    }
+  return 0;
+}
