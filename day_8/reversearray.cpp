13b93a0b473da072482790b7fa24e6001e91e4e4
itsnikhil404 <25112cn258@glbitm.ac.in>
2026-06-08 12:46:15 +0530

    reverse array using vector

---
+++ day_8/reversearray.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std ; 
+#include<vector>
+vector<int> reverse( vector<int> v){
+    int s = 0 ,e= v.size()-1;
+
+    while(s<=e){
+        swap(v[s],v[e]);
+        s++;
+        e--;    
+    }
+    return v;
+}
+void print(vector<int> ans){
+    for(int i = 0 ; i< ans.size();i++){
+        cout<<ans[i]<<" ";
+    }
+}
+
+int main(){
+    vector<int> v;
+    v.push_back(11);
+    v.push_back(7);
+    v.push_back(3);
+    v.push_back(12);
+    v.push_back(4);
+    vector<int> ans = reverse(v);
+    print(ans);
+
+}
